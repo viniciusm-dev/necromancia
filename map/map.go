8c7f613f4faca06823f8289480fc616903e4c86a
Vinicius Mesquita <vinicios.mesquita@gmail.com>
2023-02-19 21:54:23 -0300

    Commit inicial

---
+++ map/map.go
@@ -0,0 +1,15 @@
+components {
+  id: "main"
+  component: "/map/main.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
